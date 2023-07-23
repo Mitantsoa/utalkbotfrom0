7a72994dd17294364d7d1d1fbab5d8a4150d36ef
unknown <mitantsoa.andrianjaka@gmail.com>
2023-07-23 17:33:47 +0300

    test bot

--- commands/addUser.js
+++ commands/addUser.js
@@ -56,16 +56,32 @@ const action = ()=>{
             data:{
                 "title": "My Cool Modal",
                 "custom_id": "cool_modal",
-                "components": [
+                "components": [{
+                    "type": 1,
+                    "components": [{
+                        "type": 4,
+                        "custom_id": "name",
+                        "label": "Name",
+                        "style": 1,
+                        "min_length": 1,
+                        "max_length": 4000,
+                        "placeholder": "John",
+                        "required": true
+                    }]
+                },
                     {
                         "type": 1,
-                        "components": inputbuilder
-                    },
-                    {
-                        "type": 1,
-                        "components": inputbuilder
-                    }
-                ]
+                        "components": [{
+                            "type": 4,
+                            "custom_id": "name2",
+                            "label": "Name",
+                            "style": 1,
+                            "min_length": 1,
+                            "max_length": 4000,
+                            "placeholder": "John",
+                            "required": true
+                        }]
+                    }]
             }
         };
         return data;
