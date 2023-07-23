65e9e29cb7576519085ed50326038aad26819701
unknown <mitantsoa.andrianjaka@gmail.com>
2023-07-23 09:15:56 +0300

    test bot

--- index.js
+++ index.js
@@ -64,13 +64,31 @@ app.post('/interactions', async function (req, res) {
                 x(editurl)
                 // Send a message into the channel where command was triggered from
                 const data = {
-                    type: InteractionResponseType.DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE,
+                    type: InteractionResponseType.MODAL,
                     "data": {
                         "tts": false,
-                        "content": "this is from await",
-                        "embeds": [],
+                        // "content": "this is from await",
+                        // "embeds": [],
                         "allowed_mentions": { "parse": [] },
-                        "components": []
+                        "components": [
+                            {
+                                "title": "My Cool Modal",
+                                "custom_id": "cool_modal",
+                                "components": [{
+                                    "type": 1,
+                                    "components": [{
+                                        "type": 4,
+                                        "custom_id": "name",
+                                        "label": "Name",
+                                        "style": 1,
+                                        "min_length": 1,
+                                        "max_length": 4000,
+                                        "placeholder": "John",
+                                        "required": true
+                                    }]
+                                }]
+                            }
+                        ]
                     }};
                 res.send(data);
 
