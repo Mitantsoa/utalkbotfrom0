10ba447e0d34e1607a4c945f6309b0dfcd5ec920
unknown <mitantsoa.andrianjaka@gmail.com>
2023-07-23 17:37:19 +0300

    test bot

--- commands/addUser.js
+++ commands/addUser.js
@@ -4,22 +4,25 @@ const {InteractionResponseType} = require("discord-interactions");
 
 const listDBfield =[
     {id:"username",label:"Nom",required:true},
-    // {id:"firstname",label:"Prénom",required: true},
-    // {id:"uuid",label:"Matricule",required: true},
-    // {id:"adress",label:"Adress",required: false},
-    // {id:"tel",label:"Téléphone",required: false},
+    {id:"firstname",label:"Prénom",required: true},
+    {id:"uuid",label:"Matricule",required: true},
+    {id:"adress",label:"Adress",required: false},
+    {id:"tel",label:"Téléphone",required: false},
 ]
 
 const inputbuilder = listDBfield.map(v=>{
     return {
-        "type": 4,
-        "custom_id": v.id,
-        "label": v.label,
-        "style": 1,
-        "min_length": 1,
-        "max_length": 4000,
-        "placeholder": "...",
-        "required": v.required
+        type:1,
+        components:[{
+            "type": 4,
+            "custom_id": v.id,
+            "label": v.label,
+            "style": 1,
+            "min_length": 1,
+            "max_length": 4000,
+            "placeholder": "...",
+            "required": v.required
+        }]
     };
 })
 
@@ -56,32 +59,7 @@ const action = ()=>{
             data:{
                 "title": "My Cool Modal",
                 "custom_id": "cool_modal",
-                "components": [{
-                    "type": 1,
-                    "components": [{
-                        "type": 4,
-                        "custom_id": "name",
-                        "label": "Name",
-                        "style": 1,
-                        "min_length": 1,
-                        "max_length": 4000,
-                        "placeholder": "John",
-                        "required": true
-                    }]
-                },
-                    {
-                        "type": 1,
-                        "components": [{
-                            "type": 4,
-                            "custom_id": "name2",
-                            "label": "Name",
-                            "style": 1,
-                            "min_length": 1,
-                            "max_length": 4000,
-                            "placeholder": "John",
-                            "required": true
-                        }]
-                    }]
+                "components": inputbuilder
             }
         };
         return data;
