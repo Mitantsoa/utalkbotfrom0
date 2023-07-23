a91426bbff779536de3cb90b3a647f5d66c6dced
unknown <mitantsoa.andrianjaka@gmail.com>
2023-07-23 09:04:45 +0300

    test bot

--- index.js
+++ index.js
@@ -109,15 +109,23 @@ async function x(editurl) {
         "components": [
             {
                 "type": 1,
-                "components": [
-                    {
-                        "type": 2,
-                        "label": "Click me!",
-                        "style": 1,
-                        "custom_id": "click_one"
-                    }
-                ]
-            }
+                "components": {
+                    "title": "My Cool Modal",
+                    "custom_id": "cool_modal",
+                    "components": [{
+                        "type": 1,
+                        "components": [{
+                            "type": 4,
+                            "custom_id": "name",
+                            "label": "Name",
+                            "style": 1,
+                            "min_length": 1,
+                            "max_length": 4000,
+                            "placeholder": "John",
+                            "required": true
+                        }]
+                    }]
+                }
         ]
     };
     // const data = {
