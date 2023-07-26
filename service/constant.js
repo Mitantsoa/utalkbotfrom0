c88b482afad6f67f4254828dd93407b4384a8eb9
unknown <mitantsoa.andrianjaka@gmail.com>
2023-07-26 05:36:15 +0300

    test bot

--- service/constant.js
+++ service/constant.js
@@ -1,5 +1,3 @@
-const {InteractionResponseType} = require("discord-interactions");
-
 let editUrl = ""
 let createdUrl = ""
-module.exports = {editUrl,createdUrl,deferedMsg}
+module.exports = {editUrl,createdUrl}
