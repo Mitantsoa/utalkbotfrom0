b49819a58e1877ab4bbf94cc96d2bd41aa68571b
unknown <mitantsoa.andrianjaka@gmail.com>
2023-07-26 22:18:50 +0300

    test bot

--- commands/clientStrat-selectName.js
+++ commands/clientStrat-selectName.js
@@ -8,73 +8,39 @@ const action = async ()=>{
     try {
 
         const allagent = await findAll();
+        allagent.map((v,i)=>{
+            return {
+                "label": v.Agentuid,
+                "value": v.Agentuid,
+                "description": v.Agentfirstname +"-"+v.Agentlastname
+            };
+        })
 
+        console.log(allagent)
 
         // Send a message into the channel where command was triggered from
         const data = {
             type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-            // "data": {
-            //     "title":"Ajouter un agent",
-            //     "custom_id":name,
-            //     "components": [
-            //         {
-            //             "type": 1,
-            //             "components": [
-            //                 {
-            //                     "type": 4,
-            //                     "custom_id": "v.id",
-            //                     "label": "v.label",
-            //                     "style": 1,
-            //                     "min_length": 1,
-            //                     "max_length": 4000,
-            //                     "placeholder": "...",
-            //                     "required": "v.required"
-            //                 }
-            //             ]
-            //         }
-            //     ]
-            // }
             data:{
-                "content": "Mason is looking for new arena partners. What classes do you play?",
+                "content": "",
+                "embeds": [
+                    {
+                        "title": "Merci de vous identifer parmis la liste",
+                        "description": "Si vous n'est pas dans la liste merci de contacter le superviseur.\n Il est important de ne pas se trompé de nom la production sera **comptabilisé en dépendence**",
+                        "color": 5814783
+                    }
+                ],
                 "components": [
                     {
                         "type": 1,
                         "components": [
                             {
                                 "type": 3,
-                                "custom_id": "Choissez votre nom",
-                                "options":[
-                                    {
-                                        "label": "Rogue",
-                                        "value": "rogue",
-                                        "description": "Sneak n stab",
-                                        "emoji": {
-                                            "name": "rogue",
-                                            "id": "625891304148303894"
-                                        }
-                                    },
-                                    {
-                                        "label": "Mage",
-                                        "value": "mage",
-                                        "description": "Turn 'em into a sheep",
-                                        "emoji": {
-                                            "name": "mage",
-                                            "id": "625891304081063986"
-                                        }
-                                    },
-                                    {
-                                        "label": "Priest",
-                                        "value": "priest",
-                                        "description": "You get heals when I'm done doing damage",
-                                        "emoji": {
-                                            "name": "priest",
-                                            "id": "625891303795982337"
-                                        }
-                                    }
-                                ],
-                                "placeholder": "Choose a class",
+                                "custom_id": "agentUID",
+                                "options":allagent,
+                                "placeholder": "---",
                                 "min_values": 1,
-                                "max_values": 3
+                                "max_values": 1
                             }
                         ]
                     }
