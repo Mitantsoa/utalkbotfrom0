d47f0b8c27103d1e3098145325ac347dc40ba0fc
unknown <mitantsoa.andrianjaka@gmail.com>
2023-07-29 17:28:30 +0300

    test bot

--- repository/repotoken.js
+++ repository/repotoken.js
@@ -5,6 +5,7 @@ async function findtoken(id,appid){
     const sql = `SELECT interactiontoken FROM token WHERE interactionid = '${id}' and applicationid = '${appid}'`;
     console.log("find token sql:\n"+sql)
     const data = await fetch(sql);
+    console.log("find token sql respon:"+data[0])
     return data[0];
 }
 
