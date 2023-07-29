5af04014931464806d4281ad352dc35d6e6e5023
unknown <mitantsoa.andrianjaka@gmail.com>
2023-07-29 17:40:40 +0300

    test bot

--- repository/repotoken.js
+++ repository/repotoken.js
@@ -2,7 +2,7 @@ const {fetch,insert} = require('../service/mysql.js');
 
 async function findtoken(id,appid){
     // const sql = 'SELECT * FROM `agent` where `agentdiscusername` = '+username+')';
-    const sql = `SELECT interactiontoken FROM token WHERE interactionid = '${id}' and applicationid = '${appid}'`;
+    const sql = `SELECT * FROM token WHERE interactionid = '${id}' and applicationid = '${appid}'`;
     console.log("find token sql:\n"+sql)
     const data = await fetch(sql);
     console.log("find token sql respon:\n"+data[0].toString())
