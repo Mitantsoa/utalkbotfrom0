46e9183ce623204dca04b996771417e33ae78804
unknown <mitantsoa.andrianjaka@gmail.com>
2023-07-30 14:16:02 +0300

    test bot

--- commands/clientStrat-insertProd.js
+++ commands/clientStrat-insertProd.js
@@ -11,13 +11,14 @@ const action = async ({data,member})=>{
     try {
 
         const discoUser = member.user.username
+        console.log("agent id :",data)
         console.log("agent id :",data.values[0])
         console.log("discoUser:",discoUser)
         // const startprod = await startProduction(discoUser,data.values[0])
         const startprod = "this is a test";
 
         // Send a message into the channel where command was triggered from
-        const data = {
+        const dataresp = {
             type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
             data:{
                 "content": "",
@@ -32,7 +33,7 @@ const action = async ({data,member})=>{
             }
         };
         // await createInterResp(data)
-        return data;
+        return dataresp;
 
     }catch (e) {
         console.log(e)
